739822711d1e8dd918f290d5fe0926680e5d2d89
Agip003 <blessedmuchemi@gmail.com>
2026-04-28 16:42:10 +0300

    day 4-5: admin_required decorator, RBAC, admin routes protected

--- app/middleware.py
+++ app/middleware.py
@@ -14,20 +14,24 @@ def login_required(f):
     def decorated_function(*args, **kwargs):
         auth = request.headers.get("Authorization", "")
 
-        #Check if it starts with Bearer
-        if not auth or not auth.startswith("Bearer"):
+        #Check if it starts  with Bearer
+        if not auth or not auth.startswith("Bearer "):
             abort(401, description = "Invalid or missing Authorization")
 
         #Extract the token
-        token = auth.split(" ")[1]
+        parts = auth.split(" ")
+        if len(parts) != 2:
+            abort(401, description="Invalid Authorization header format")
 
+        token = parts[1]
+        
         try:
-            payload = jwt.decode(token, SECRET_KEY, algorithm="HS256")
+            payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
         
         except jwt.ExpiredSignatureError as e:
             abort(401, description="Token has expired")
         except jwt.InvalidTokenError as e:
-            abort(401, descripiton="Invalid token") 
+            abort(401, description="Invalid token") 
 
         g.current_user = {
             "user_id": payload["user_id"],
@@ -37,3 +41,23 @@ def login_required(f):
 
         return f(*args, **kwargs)
     return decorated_function
+
+def admin_required(f):
+    """Decorators to restrict routes to admin users only"""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+
+        if not hasattr(g, 'current_user'):
+            abort(401, "Authentication required")
+
+        
+        user_role = g.current_user.get("role")
+        if  not isinstance(g.current_user, dict):
+            abort(403, description="Invalid data format")
+                 
+        if user_role != "admin":
+            abort(403, description="Admin access required")
+
+        return f(*args, **kwargs)
+    return decorated_function
+        
